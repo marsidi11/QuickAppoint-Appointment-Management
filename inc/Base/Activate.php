0987d27ef246dc71896ed6db5d942544deddfa47
Marsidi <marsidzyberi9@gmail.com>
2024-04-06 11:21:13 +0200

    create sql table on activation

--- inc/Base/Activate.php
+++ inc/Base/Activate.php
@@ -14,13 +14,18 @@
 
       $sql = "CREATE TABLE $table_name 
       (
-         id mediumint(9) NOT NULL AUTO_INCREMENT,
-         name tinytext NOT NULL,
-         date datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-         PRIMARY KEY  (id)
+         id mediumint(9) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+         name varchar(255) NOT NULL,
+         surname varchar(255) NOT NULL,
+         phone varchar(20) NOT NULL,
+         email varchar(255) NOT NULL,
+         services TEXT NOT NULL,
+         date date NOT NULL,
+         startTime time NOT NULL,
+         endTime time NOT NULL
       ) $charset_collate;";
 
-      require_once(\ABSPATH . 'wp-admin/includes/upgrade.php');
-      \dbDelta($sql);
+      require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+      dbDelta($sql);
     }
  }
