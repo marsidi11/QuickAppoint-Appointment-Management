6274d0e7c2a7fd697055525fdfa7750edf260893
Marsidi <marsidzyberi9@gmail.com>
2024-04-13 13:23:34 +0200

    better table format

--- assets/admin/js/src/appointments/index.vue
+++ assets/admin/js/src/appointments/index.vue
@@ -3,8 +3,8 @@
     <div class="relative overflow-x-auto">
       <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <tr :class="theadRowClass">
-            <th v-for="column in columns" :key="column" scope="col" class="px-6 py-3">
+          <tr>
+            <th v-for="column in columns" :key="column" scope="col" class="px-6 py-3 uppercase">
               {{ column }}
             </th>
           </tr>
