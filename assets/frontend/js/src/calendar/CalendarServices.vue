c15c275f65406cc3d43c8fbd845b5176171f367c
Marsidi <marsidzyberi9@gmail.com>
2024-05-18 01:10:33 +0200

    responsive style update

--- assets/frontend/js/src/calendar/CalendarServices.vue
+++ assets/frontend/js/src/calendar/CalendarServices.vue
@@ -52,8 +52,14 @@ export default {
                     return 'grid-cols-2';
                 case 3:
                     return 'grid-cols-2 md:grid-cols-3';
+                case 4:
+                    return 'grid-cols-2 md:grid-cols-3 lg:grid-cols-2';
+                case 5:
+                    return 'grid-cols-2 md:grid-cols-3 lg:grid-cols-3';
+                case 6:
+                    return 'grid-cols-2 md:grid-cols-3 lg:grid-cols-3';
                 default:
-                    return 'grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5';
+                    return 'grid-cols-2 md:grid-cols-3 lg:grid-cols-4';
             }
         }
     },
